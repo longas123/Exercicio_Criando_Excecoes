bbb9bae54a6e645932a66af49236788c106b65ae
longas123 <mateusmontano@gmail.com>
2020-03-26 18:28:16 -0300

    Modelo de Excessões, exercício simples

---
+++ src/Application/Program.java
@@ -0,0 +1,55 @@
+package Application;
+
+import java.text.ParseException;
+import java.util.Locale;
+import java.util.Scanner;
+
+import Entities.Account;
+import model.Exception.DomainExceptions;
+
+public class Program {
+
+	public static void main(String[] args) throws ParseException {
+		try (Scanner sc = new Scanner(System.in)) {
+			Locale.setDefault(Locale.US);
+			
+//		System.out.println("Enter account data");
+//		System.out.print("Number: ");
+//		int number = sc.nextInt();
+//		System.out.print("Holder: ");
+//		sc.nextLine();
+//		String holder = sc.nextLine();
+//		System.out.print("Initial Balance: ");
+//		double balance = sc.nextDouble();
+//		System.out.print("Withdraw limit: ");
+//		double withDrawLimit = sc.nextDouble();
+//		System.out.println();
+			System.out.println("Enter account data");
+			System.out.print("Number: ");
+			int number = sc.nextInt();
+			System.out.print("Holder: ");
+			sc.nextLine();
+			String holder = sc.nextLine();
+			System.out.print("Initial balance: ");
+			double balance = sc.nextDouble();
+			System.out.print("Withdraw limit: ");
+			double withdrawLimit = sc.nextDouble();
+			
+			
+			Account acount = new Account(number, holder, balance, withdrawLimit);
+			
+			System.out.print("Enter amount for withdraw: ");
+			double amount  = sc.nextDouble();
+			
+			try {
+				acount.withDraw(amount);
+				System.out.println("New balance: " + acount);
+			}
+			catch (DomainExceptions e) {
+				System.out.println("Withdraw error: " + e.getMessage());
+			}
+		}
+
+	}
+
+}
---
+++ src/Entities/Account.java
@@ -0,0 +1,62 @@
+package Entities;
+
+import model.Exception.DomainExceptions;
+
+public class Account{
+	
+	private Integer number;
+	private String holder;
+	private Double balance;
+	private Double withDrawLimit;
+
+	
+	
+	public Account() {
+		super();
+	}	
+	
+	public Account(Integer number, String holder, Double balance, Double withDrawLimit) {
+		super();
+		this.number = number;
+		this.holder = holder;
+		this.balance = balance;
+		this.withDrawLimit = withDrawLimit;
+	}
+
+	public Integer getNumber() {
+		return number;
+	}
+	public void setNumber(Integer number) {
+		this.number = number;
+	}
+	public String getHolder() {
+		return holder;
+	}
+	public void setHolder(String holder) {
+		this.holder = holder;
+	}
+	public Double getBalance() {
+		return balance;
+	}
+	public Double getWithDrawLimit() {
+		return withDrawLimit;
+	}
+	public void  depositAmount(Double amount) {
+		balance += amount;
+	}
+	public void	withDraw(double amount) throws DomainExceptions{
+		if(withDrawLimit < amount) {
+			throw new DomainExceptions("The amount exceeds withdraw limit");
+		}else if(amount < balance) {
+			throw new DomainExceptions("Not enough balance");
+		}	
+		balance -= amount;
+	}
+	
+	@Override
+	public String toString() {
+		return "New balance: "
+				+ String.valueOf(getBalance());
+	}
+
+}
